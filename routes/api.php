87f024d1a9a4cf6c35a579b447ac37a96cfe99e6
aditiya1216 <aditiyasulistiyani12@gmail.com>
2026-08-27 16:55:43 +0700

    add rate limiter in route

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 // Auth routes
 Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-        // ->middleware('throttle:3,60');
+    Route::post('/register', [AuthController::class, 'register'])
+        ->middleware('throttle:3,60');
     
-    Route::post('/login', [AuthController::class, 'login']);
-        // ->middleware('throttle:3,60');
+    Route::post('/login', [AuthController::class, 'login'])
+        ->middleware('throttle:3,60');
     
     Route::post('/logout', [AuthController::class, 'logout'])
         ->middleware('auth:sanctum');
